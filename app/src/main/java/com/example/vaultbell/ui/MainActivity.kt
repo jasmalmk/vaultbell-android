13c425340093bd4a3d10ca9c420d3ebecde09ebd
Jasmal MK <jasmal.mk@gmail.com>
2025-08-11 23:12:00 +0400

    Initial skeleton: MainActivity

---
+++ app/src/main/java/com/example/vaultbell/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vaultbell.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.vaultbell.ui.screens.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val mainViewModel: MainViewModel = viewModel()
+            HomeScreen(viewModel = mainViewModel)
+        }
+    }
+}
